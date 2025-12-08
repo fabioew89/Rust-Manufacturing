3ec8cad354ae1f3f6455e8bc94fa66fbff1e9cf8
Fabio Ewerton <fabioew89@gmail.com>
2025-12-08 15:35:44 -0400

    feat: add a header content

---
+++ src/app/components/header/header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Navigation } from '../navigation/navigation';
+
+@Component({
+  selector: 'app-header',
+  imports: [Navigation],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+
+}
