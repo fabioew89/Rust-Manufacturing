c850e10fb1e033cae69d530636aa28f577de1bba
Fabio Ewerton <fabioew89@gmail.com>
2025-12-15 15:09:56 -0400

    fix: fixed routes login and dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './core/components/login/login';
+import { Dashboard } from './core/components/dashboard/dashboard';
 
 export const routes: Routes = [
-    { path: "login", component: Login, title: "Login"}
+    { path: "login", component: Login, title: "Login"},
+    { path: "", redirectTo: "login", pathMatch: "full"},
+    { path: "dashboard", component: Dashboard, title: "Dashboard"},
 ];
