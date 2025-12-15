54766605cfcab00e4271d05800567dbf0a4dbeb9
Fabio Ewerton <fabioew89@gmail.com>
2025-12-15 16:36:01 -0400

    feat: add page create-new-items

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './core/components/login/login';
 import { Dashboard } from './core/components/dashboard/dashboard';
+import { CreateNewItems } from './pages/create-new-items/create-new-items';
 
 export const routes: Routes = [
     { path: "login", component: Login, title: "Login"},
     { path: "", redirectTo: "login", pathMatch: "full"},
     { path: "dashboard", component: Dashboard, title: "Dashboard"},
+    { path: "create", component: CreateNewItems, title: "Create"},
 ];
---
+++ src/app/pages/create-new-items/create-new-items.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-items',
+  imports: [],
+  templateUrl: './create-new-items.html',
+  styleUrl: './create-new-items.css',
+})
+export class CreateNewItems {
+
+}
