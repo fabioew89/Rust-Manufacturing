dabaf68b376feb491bdf3cdc09d3551a71b10119
Leonardo <lptleo11@gmail.com>
2025-12-17 16:56:38 -0400

    feat: item creation page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './core/components/login/login';
 import { Dashboard } from './core/components/dashboard/dashboard';
+import { Create } from './core/components/create/create';
 
 export const routes: Routes = [
     { path: "login", component: Login, title: "Login"},
     { path: "", redirectTo: "login", pathMatch: "full"},
     { path: "dashboard", component: Dashboard, title: "Dashboard"},
+    { path: "create", component: Create, title: "Create tracked item"}
 ];
