27d7f269667827394adc2bd6fc421a3e379f9268
Fabio Ewerton <fabioew89@gmail.com>
2025-12-18 14:29:15 -0400

    feat: add create-user page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Create } from './pages/create/create';
+import { CreateUser } from './pages/create-user/create-user';
 
 export const routes: Routes = [
     { path: "login", component: Login, title: "Login"},
     { path: "", redirectTo: "login", pathMatch: "full"},
     { path: "dashboard", component: Dashboard, title: "Dashboard"},
     { path: "create", component: Create, title: "Create"},
+    {path: "create-user", component: CreateUser, title: "Create User"},
 ];
