72ea0281750b5282aa1bc30186a5fae61e45d962
Leonardo <lptleo11@gmail.com>
2025-12-19 03:04:02 -0400

    feat: add login form validation

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,13 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule} from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  formBuilder = inject(FormBuilder);
 
+  formLogin = this.formBuilder.group({
+    login: ['', [Validators.required, Validators.minLength(6)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  })
+
+  get isFormLoginInvalid(): boolean {
+    const login = this.formLogin.get('login')
+    const password = this.formLogin.get('password')
+
+    const isLoginInvalid = login?.invalid && login?.touched;
+    const isPasswordInvalid = password?.invalid && password?.touched;
+
+    return !!(isLoginInvalid || isPasswordInvalid)
+  }
 }
