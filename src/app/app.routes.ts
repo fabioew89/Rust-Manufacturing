29bd32d028d0a03ed8a7d40c80ae12dd8e16743e
Leonardo <lptleo11@gmail.com>
2025-12-22 15:12:55 -0400

    feat(item): add item page with layout and routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Create } from './pages/create/create';
 import { CreateUser } from './pages/create-user/create-user';
 import { authGuard } from './core/guards/auth.guard';
 import { publicGuard } from './core/guards/public.guard';
+import { Item } from './pages/item/item';
 
 export const routes: Routes = [
     { path: "login", component: Login, title: "Login", canActivate: [publicGuard] },
@@ -12,4 +13,5 @@ export const routes: Routes = [
     { path: "dashboard", component: Dashboard, title: "Dashboard", canActivate: [authGuard] },
     { path: "create", component: Create, title: "Create", canActivate: [authGuard] },
     { path: "create-user", component: CreateUser, title: "Create User", canActivate: [authGuard] },
+    { path: "item", component: Item, title: "Item Info", canActivate: [authGuard] },
 ];
